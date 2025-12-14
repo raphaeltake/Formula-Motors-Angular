989c2b7b67b7c69bea3a5a53aaf8712bf9cd16ec
raphaeltake <raph.takeuchi@gmail.com>
2025-12-14 02:29:25 -0300

    Finalização da página home e adição de icones no Header

--- formula/src/app/app.routes.ts
+++ formula/src/app/app.routes.ts
@@ -1,17 +1,18 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { VeiculosComponent } from './veiculos/veiculos.component';
-import { FuncionariosComponent } from './funcionarios/funcionarios.component';
-import { CadastrarVeiculosComponent } from './cadastrar-veiculos/cadastrar-veiculos.component';
-import { CadastrarFuncionariosComponent } from './cadastrar-funcionarios/cadastrar-funcionarios.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { VeiculosComponent } from './pages/veiculos/veiculos.component';
+import { FuncionariosComponent } from './pages/funcionarios/funcionarios.component';
+import { CadastrarVeiculosComponent } from './pages/cadastrar-veiculos/cadastrar-veiculos.component';
+import { CadastrarFuncionariosComponent } from './pages/cadastrar-funcionarios/cadastrar-funcionarios.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: "home", component: HomeComponent },
   { path: "veiculos", component: VeiculosComponent },
-  { path: "funcionarios", component: FuncionariosComponent },
+  { path: "funcionarios", component: FuncionariosComponent},
   { path: "cadastrar_veiculo", component: CadastrarVeiculosComponent },
   { path: "cadastrar_funcionario", component: CadastrarFuncionariosComponent },
   { path: "login", component: LoginComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
--- formula/src/app/components/header/header.component.ts
+++ formula/src/app/components/header/header.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { RouterLink } from "@angular/router";
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, RouterLink],
+  imports: [MatToolbarModule, RouterLink, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   logado: boolean = false
+
+  setLogado(){ //apenas para testes
+    this.logado = !this.logado
+  }
 }
